2f20b0ef5f076bec713f124e8d51a88f88b90c85
gcc2ge <1414786333@qq.com>
2019-05-05 16:51:02 +0800

    limit http connection to geth node

--- sender_txs/sender.js
+++ sender_txs/sender.js
@@ -9,6 +9,7 @@ var Web3 = require("web3");
 
 var Wallet = require('../lib/wallet');
 
+const queue_size = 600;
 
 //
 
@@ -21,19 +22,36 @@ async function start(web3, pause = 50, wallet_Private, toArr) {
     let count = 0;
     let toLen = toArr.length;
 
+    let hashs = new Map();
+
     let timer = setIntervalAsync(
         async () => {
 
-            let to = toArr[count % toLen];
-            count = count + 1;
 
             let amount = '0.00000000000001'; //ether
 
-            coin.transfer(wallet_Private.getAddressString(), to.getAddressString(), amount, nonce, (err) => {
-                console.info(`${err}`);
-                clearIntervalAsync(timer);
+            if (hashs.size <= queue_size) {//控制http请求数量
+                let to = toArr[count % toLen];
+                count = count + 1;
+
+                coin.transfer(wallet_Private.getAddressString(), to.getAddressString(), amount, nonce, (err) => {
+                    console.info(`${err}`);
+                    clearIntervalAsync(timer);
+                }, (hash) => {
+                    hashs.set(hash, 1);
+                });
+                nonce = nonce + 1;
+            }
+
+            // 删除成功的交易
+            hashs.forEach((item, key, mapObj) => {
+                web3.eth.getTransactionReceipt(hash[i]).then(hash => {
+                    if (hash) {
+                        hashs.delete(hash);
+                    }
+                })
             });
-            nonce = nonce + 1;
+
         },
         pause
     );
